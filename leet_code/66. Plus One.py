322923b8669ab71a57b7299272c9fe60374f1864
Zaminur151 <hasanzminur@gmail.com>
2024-12-25 11:10:55 +0600

    adding leet code folder

---
+++ leet_code/66. Plus One.py
@@ -0,0 +1,33 @@
+
+# main solution
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        
+        for i in range(len(digits)-1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            
+            digits[i] = 0
+
+        return [1] + digits
+
+
+
+
+# total syntex
+from typing import List
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        
+        for i in range(len(digits)-1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            
+            digits[i] = 0
+
+        return [1] + digits
+solution = Solution()
+print(solution.plusOne([9,9,9]))
